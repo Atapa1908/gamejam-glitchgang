bb3297e4371f6f39de879c9c324882b44899afc9
RAllen036 <ryanallen783@gmail.com>
2024-07-14 02:18:03 +0100

    Untested inventory
    Waiting on assets before testing

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody2D
 
+# NOTE
+# Might need to move some base functions into a base class
+
 @onready var collision_shape: CollisionShape2D = $CollisionShape
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite
+@onready var camera: Camera2D = $Camera
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export_category("Statistics")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export_category("Movement")
+
+
 func _physics_process(delta: float) -> void:
-	pass
+	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	
+	velocity = direction * 200.0
+
+func heal(_heal_value: float) -> void:
+	print("Healing.")
