42d7a3420c7e3e2fc9a2557b4b677a1e387b7200
Estachen <atapa@hotmail.de>
2024-07-16 23:40:00 +0200

    platformer adjustments

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,72 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-var current_direction = "none"
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-func _ready():
-	$AnimatedSprite2D.play('front_idle')
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	player_movement(delta)
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-func player_movement(delta):
-	if Input.is_action_pressed("ui_right"):
-		current_direction = "right"
-		play_animation(1)
-		velocity.x = SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_direction = "left"
-		play_animation(1)
-		velocity.x = -SPEED
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_up"):
-		current_direction = "up"
-		play_animation(1)
-		velocity.x = 0
-		velocity.y = -SPEED
-	elif Input.is_action_pressed("ui_down"):
-		current_direction = "down"
-		play_animation(1)
-		velocity.x = 0
-		velocity.y = SPEED
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
 	else:
-		play_animation(0)
-		velocity.x = 0
-		velocity.y = 0
-		
-	move_and_slide()
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-func play_animation(movement):
-	var direction = current_direction
-	var animation = $AnimatedSprite2D
-	
-	if direction == "right":
-		animation.flip_h = false
-		if movement == 1:
-			animation.play("side_walk")
-		elif movement == 0:
-			animation.play("side_idle")
-	if direction == "left":
-		animation.flip_h = true
-		if movement == 1:
-			animation.play("side_walk")
-		elif movement == 0:
-			animation.play("side_idle")
-			
-	if direction == "up":
-		animation.flip_h = true
-		if movement == 1:
-			animation.play("back_walk")
-		elif movement == 0:
-			animation.play("back_idle")
-		
-	if direction == "down":
-		animation.flip_h = true
-		if movement == 1:
-			animation.play("front_walk")
-		elif movement == 0:
-			animation.play("front_idle")
-		
+	move_and_slide()
