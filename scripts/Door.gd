5e634b74eaa68792968275d0fad45e37cb139301
RAllen036 <ryanallen783@gmail.com>
2024-07-22 20:09:48 +0100

    Transition base layed

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -5,5 +5,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file.call_deferred("res://scenes/game.tscn")
-		#SceneManager.transition(scene, scene_door_name)
+		SceneManager.transition(scene, scene_door_name)
