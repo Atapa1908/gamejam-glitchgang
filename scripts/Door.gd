d85198e755615520093ce7c70f649ff5bd11b03a
RAllen036 <ryanallen783@gmail.com>
2024-07-25 01:45:18 +0100

    Music transition functions
    Also normalisation of world data stored in scene manager

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
-@export_file var scene: String
+@export var scene_name: String
 @export var scene_door_name: String
 
 func _on_body_entered(body: Node2D) -> void:
-	if Engine.is_editor_hint():
-		return
 	if body.is_in_group("player"):
-		SceneManager.transition(scene, scene_door_name)
+		SceneManager.transition(scene_name, scene_door_name)
